d5df15f288348a0136f19a10601bca7d31f8bdc4
Reginald Osano <osanoreginald@gmail.com>
2020-02-23 14:05:45 +0800

    Updates, changes - Yehey!

---
+++ src/db/db.js
@@ -0,0 +1,44 @@
+module.exports = () => {
+  return [
+    {
+      lastName: "Phillips",
+      firstName: "Mike",
+      address: "Portland, Oregon",
+      memberType: "Grower",
+      email: "mikephillips@yahoo.com",
+      password: "mike43yes"
+    },
+    {
+      lastName: "De La Cruz",
+      firstName: "Juan",
+      address: "UST, Manila",
+      memberType: "Grower",
+      email: "delacruzjuan@gmail.com",
+      password: "jdlcforever12"
+    },
+    {
+      lastName: "Smith",
+      firstName: "Rex",
+      address: "Maine, Oregon",
+      memberType: "Grower",
+      email: "smithrex@companyxyz.com",
+      password: "rex123"
+    },
+    {
+      lastName: "Lim",
+      firstName: "Jenny",
+      address: "Kentucky, Oregon",
+      memberType: "Hauler",
+      email: "missjenny@parity.com",
+      password: "jenny#ok78"
+    },
+    {
+      lastName: "Perez",
+      firstName: "Prado",
+      address: "Project 8, Quezon City",
+      memberType: "Hauler",
+      email: "pradz@yahoo.com",
+      password: "pp456"
+    }
+  ];
+};
--- src/routes/registration-login-management/login.js
+++ src/routes/registration-login-management/login.js
@@ -1,12 +1,31 @@
 import express from "express";
 
+import db from "../../db/db";
+
 const router = express.Router();
+
+const members = db();
 
 router.post("/login", async (req, res) => {
   try {
-    res.status(200).json({
-      data: req.body
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const searchMember = members.find(m => {
+      return m.email === email && m.password === password;
     });
+
+    if (searchMember) {
+      res.status(200).json({
+        isSuccessfull: true,
+        data: searchMember
+      });
+    } else {
+      return res.status(400).json({
+        isSuccessfull: true,
+        message: "Invalid Credentials!"
+      });
+    }
   } catch (error) {
     return res.status(500).send({
       error: error

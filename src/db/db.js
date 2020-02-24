8560242ec171688b63b210ae479bdd94bc63e0c6
Reginald Osano <osanoreginald@gmail.com>
2020-02-24 08:20:14 +0800

    Update as of Feb. 24

--- src/db/db.js
+++ src/db/db.js
@@ -1,42 +1,52 @@
 module.exports = () => {
   return [
     {
-      lastName: "Phillips",
-      firstName: "Mike",
+      accountID: "Phillips",
+      accountName: "Mike Phillips",
       address: "Portland, Oregon",
-      memberType: "Grower",
+      type: "Grower",
       email: "mikephillips@yahoo.com",
+      phoneNumber: "",
+      hauler_grower: "",
       password: "mike43yes"
     },
     {
-      lastName: "De La Cruz",
-      firstName: "Juan",
+      accountID: "De La Cruz",
+      accountName: "Juan De La Cruz",
       address: "UST, Manila",
-      memberType: "Grower",
+      type: "Grower",
       email: "delacruzjuan@gmail.com",
+      phoneNumber: "",
+      hauler_grower: "",
       password: "jdlcforever12"
     },
     {
-      lastName: "Smith",
-      firstName: "Rex",
+      accountID: "Smith",
+      accountName: "Rex Smith",
       address: "Maine, Oregon",
-      memberType: "Grower",
+      type: "Grower",
       email: "smithrex@companyxyz.com",
+      phoneNumber: "",
+      hauler_grower: "",
       password: "rex123"
     },
     {
-      lastName: "Lim",
-      firstName: "Jenny",
+      accountID: "Lim",
+      accountName: "Jenny Lim",
       address: "Kentucky, Oregon",
-      memberType: "Hauler",
+      type: "Hauler",
       email: "missjenny@parity.com",
+      phoneNumber: "",
+      hauler_grower: "",
       password: "jenny#ok78"
     },
     {
-      lastName: "Perez",
-      firstName: "Prado",
+      accountID: "Perez",
+      accountName: "Perez Prado",
       address: "Project 8, Quezon City",
-      memberType: "Hauler",
+      type: "Hauler",
+      phoneNumber: "",
+      hauler_grower: "",
       email: "pradz@yahoo.com",
       password: "pp456"
     }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 7700;
 
 server(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, error => {
+  if (error) {
+    alert(`There was an error - ${error}`);
+    return;
+  }
   console.log(`Server started on port ${PORT}...`);
 });
